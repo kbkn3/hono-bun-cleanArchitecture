4e4662897dabc18ca3672ca0a328ed028bce9667
Wabu-K <KobayashiKenta@lifull.com>
2024-03-05 17:46:59 +0900

    docs: fix readme

--- src/router/routing.config.ts
+++ src/router/routing.config.ts
@@ -7,15 +7,15 @@ import { CONTROLLER_BINDINGS } from '@/keys';
 
 export const routings: Route[] = [
   {
-    name: 'hello.world',
-    serviceName: CONTROLLER_BINDINGS.HelloWorld,
-    path: '/message/:message',
-    methods: ['get'],
-  },
-  {
     name: 'pokemon',
     serviceName: CONTROLLER_BINDINGS.Pokemon,
     path: '/pokemon/:id',
+    methods: ['get'],
+  },
+  {
+    name: 'hello.world',
+    serviceName: CONTROLLER_BINDINGS.HelloWorld,
+    path: '/:message',
     methods: ['get'],
   }
 ];
